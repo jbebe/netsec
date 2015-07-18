5bf7ebbe8771cccf8ef49939b1bd6221672f86c7
Juhász Bálint <juhasz.balint.bebe@gmail.com>
2015-07-18 20:15:27 +0200

    added full log, TODO: exit

--- Netsec/PacketConsumer.hpp
+++ Netsec/PacketConsumer.hpp
@@ -30,8 +30,11 @@ class PacketConsumer {
 	// plugins
 	ParsedPacketElem parsed_elem;
 	
+	// debug
+	std::stringstream dbg_str;
+	
 public:
-	PacketConsumer(){}
+	PacketConsumer(): dbg_str{} {}
 	
 	PacketConsumer(PacketConsumer &&moved_consumer): PacketConsumer() {}
 	
@@ -52,8 +55,8 @@ public:
 	void pluginsIPv4(iphdr *data){
 		uint32_t ip_addr = ntohl(data->saddr);
 		struct _ipa { uint8_t d, c, b, a; };
-		_ipa *padded_ip = reinterpret_cast<_ipa*>(&ip_addr);
-		dbg_printf("%d.%d.%d.%d", padded_ip->a, padded_ip->b, padded_ip->c, padded_ip->d);
+		_ipa *padded_ip = (_ipa*)(&ip_addr);
+		dbg_str << 1*(padded_ip->a) << "." << 1*(padded_ip->b) << "." << 1*(padded_ip->c) << "." << 1*(padded_ip->d);
 	}
 	
 	void pluginsIPv6(ip6_hdr *data){
@@ -61,7 +64,7 @@ public:
 	}
 	
 	void pluginsTCP(tcphdr *data){
-		dbg_printf(":%d", ntohs(data->dest));
+		dbg_str << ":" << ntohs(data->dest);
 	}
 	
 	void pluginsUDP(udphdr *data){
@@ -69,42 +72,52 @@ public:
 	}
 	
 	void pluginsAPP(uint8_t *data){
-		
+		dbg_str << " - ";
+		for (int i = 0; i < 40; i++){
+			char out;
+			if (data[i] >= 32 && data[i] <= 126){
+				out = data[i];
+			}
+			else {
+				out = '.';
+			}
+			dbg_str << out;
+		}
 	}
 	
 	void parse(RawPacketElem *data){
 		uint8_t *tcp_layer;
-		iphdr *ip_layer = reinterpret_cast<iphdr*>(data->getData());
+		iphdr *ip_layer = (iphdr*)(data->getData());
 		uint8_t protocol;
 		
 		switch (ip_layer->version){
 			case IPV4_VER:
-				tcp_layer = reinterpret_cast<uint8_t*>(ip_layer) + (ip_layer->ihl * 4);
+				tcp_layer = (uint8_t*)ip_layer + (ip_layer->ihl * 4);
 				protocol = ip_layer->protocol;
 				pluginsIPv4(ip_layer);
 				break;
 			case IPV6_VER:
-				tcp_layer = reinterpret_cast<uint8_t*>(ip_layer) + sizeof(ip6_hdr);
-				protocol = reinterpret_cast<ip6_hdr*>(ip_layer)->ip6_ctlun.ip6_un1.ip6_un1_nxt;
-				pluginsIPv6(reinterpret_cast<ip6_hdr*>(ip_layer));
+				tcp_layer = (uint8_t*)ip_layer + sizeof(ip6_hdr);
+				protocol = ((ip6_hdr*)ip_layer)->ip6_ctlun.ip6_un1.ip6_un1_nxt;
+				pluginsIPv6((ip6_hdr*)ip_layer);
 				break;
 			default:
 				tcp_layer = nullptr;
 				protocol = 0;
 				break;
 		}
-		if (tcp_layer == nullptr || protocol == 0) return;
+		if (protocol == 0 || tcp_layer == nullptr) return;
 		
 		uint8_t *app_layer;
 		
 		switch (protocol){
 			case IPPROTO_TCP:
-				app_layer = reinterpret_cast<uint8_t*>(tcp_layer) + (reinterpret_cast<tcphdr*>(tcp_layer)->doff * 4);
-				pluginsTCP(reinterpret_cast<tcphdr*>(tcp_layer));
+				app_layer = (uint8_t*)tcp_layer + (((tcphdr*)tcp_layer)->doff * 4);
+				pluginsTCP((tcphdr*)tcp_layer);
 				break;
 			case IPPROTO_UDP:
-				app_layer = reinterpret_cast<uint8_t*>(tcp_layer) + sizeof(udphdr);
-				pluginsUDP(reinterpret_cast<udphdr*>(tcp_layer));
+				app_layer = (uint8_t*)tcp_layer + sizeof(udphdr);
+				pluginsUDP((udphdr*)tcp_layer);
 				break;
 			default: 
 				app_layer = nullptr;
@@ -120,8 +133,12 @@ public:
 			RawPacketElem data;
 			get(&data);
 			parse(&data);
-			dbg_printf("\n");
+			
+			dbg_printf("%s\n", dbg_str.str().c_str());
+			dbg_str.str(std::string{});
+			dbg_str.clear();
 		}
 	}
 	
 };
+
